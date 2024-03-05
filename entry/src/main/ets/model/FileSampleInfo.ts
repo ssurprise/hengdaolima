6001653dd3cfb67fd8a97c92caa77d2ac246a7ed
shiguotao <shiguotao-os@360os.com>
2024-03-05 21:02:25 +0800

    更新：新增文件管理、读写操作

---
+++ entry/src/main/ets/model/FileSampleInfo.ts
@@ -0,0 +1,12 @@
+/**
+ * @Author： shiguotao
+ * @E-mail: skxdad@163.com
+ * @DATE： 2024/3/5 15:30
+ * @Description： 文件基本信息
+ */
+export class FileSampleInfo {
+  path: string
+  name: string
+  size: number
+  type: string
+}
